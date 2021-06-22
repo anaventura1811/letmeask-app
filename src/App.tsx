244cf7928d3ce8950feadbdbd40ca86d3515a6f1
Ana Ventura <anaventura1811@gmail.com>
2021-06-21 23:39:42 -0300

    notes: React core concepts

--- src/App.tsx
+++ src/App.tsx
@@ -15,3 +15,5 @@ export default App;
 // Componente no React = função que devolve um HTML
 // Novas APIs do React só funcionam em componentes funcionais, isto é, em componentes feitos com funções 
 // e não com classes
+// Propriedades - React: informações ou atributos que passamos aos componentes para que
+// eles possam se comportar de maneira diferente
