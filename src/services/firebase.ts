3041f30e205c215951f8acbf22bdcd1f66e3d95b
Ana Ventura <anaventura1811@gmail.com>
2021-06-22 00:12:13 -0300

    feat: firebase settings

---
+++ src/services/firebase.ts
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZMG6gePLXPTVcwLwEnrl1QAxCBFfVgQA",
+  authDomain: "letmeask-app-75bba.firebaseapp.com",
+  databaseURL: "https://letmeask-app-75bba-default-rtdb.firebaseio.com",
+  projectId: "letmeask-app-75bba",
+  storageBucket: "letmeask-app-75bba.appspot.com",
+  messagingSenderId: "2419008624",
+  appId: "1:2419008624:web:a703231d2c98338ba140b5",
+  measurementId: "G-M2JQLE1QXT"
+};
+
+firebase.initializeApp(firebaseConfig);
