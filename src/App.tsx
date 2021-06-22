185e311f9be352a61b806daafd600ce29cd2fd5e
Ana Ventura <anaventura1811@gmail.com>
2021-06-21 23:37:54 -0300

    notes: React core concepts

--- src/App.tsx
+++ src/App.tsx
@@ -11,3 +11,7 @@ export default App;
 
 // Fast refresh  - React - permite que a aplicação tenha sua interface atualizada,
 // sem necessidade de reload contínuo do usuário
+// Componentes = partes isoladas de código que, uma vez juntas, compõem a aplicação
+// Componente no React = função que devolve um HTML
+// Novas APIs do React só funcionam em componentes funcionais, isto é, em componentes feitos com funções 
+// e não com classes
