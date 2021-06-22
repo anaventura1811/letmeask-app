2868152966c175de5a001b130e03df11ad14ecc6
Ana Ventura <anaventura1811@gmail.com>
2021-06-21 23:55:34 -0300

    notes: component example, React core concepts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Button from "./components/Button";
 
 function App() {
   return (
-    <h1 className="App">
-      Hello World
-    </h1>
+    <>
+      <h1 className="App">
+        Hello World
+      </h1>
+      <Button></Button>
+    </>
   );
 }
 
