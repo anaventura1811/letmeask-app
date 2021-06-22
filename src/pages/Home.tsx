2e77bc5ba3eb175f7b2e8e07bf5c2ce26463472b
Ana Ventura <anaventura1811@gmail.com>
2021-06-22 11:04:32 -0300

    feat: fixing lint, styles in alphabetical order

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,11 +14,11 @@ function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button type="button">
+          <button type="button" className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
             <input
               type="text"
