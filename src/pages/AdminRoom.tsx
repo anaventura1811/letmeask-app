9bcfa77baa99ab00cb4cdea734f1190c9b0df971
Ana Ventura <anaventura1811@gmail.com>
2021-06-25 13:03:25 -0300

    feat: implements mark question as answered & highlight question functionalities

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,6 +1,8 @@
 import { useHistory, useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
 import Button from '../components/Button';
 import RoomCode from '../components/RoomCode';
 import '../styles/room.scss';
@@ -32,6 +34,18 @@ function AdminRoom() {
     }
   }
 
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+     await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
+  }
+
   return (
     <div id="page-room">
       <header>
@@ -59,6 +73,18 @@ function AdminRoom() {
               content={ question.content}
               author={ question.author }
             >
+              <button
+                type="button"
+                onClick={() => handleCheckQuestionAsAnswered(question.id)}
+              >
+                <img src={checkImg} alt="Marcar pergunta como respondida" />
+              </button>
+              <button
+                type="button"
+                onClick={() => handleHighlightQuestion(question.id)}
+              >
+                <img src={answerImg} alt="Dar destaque à pergunta" />
+              </button>
               <button
                 type="button"
                 onClick={() => handleDeleteQuestion(question.id)}
